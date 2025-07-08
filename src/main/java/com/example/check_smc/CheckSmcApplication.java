08c3a8a71f9f1a36a85daef18aed6dabce253608
Van Hoang <nbdev82@gmail.com>
2025-07-08 11:48:46 +0700

    init

---
+++ src/main/java/com/example/check_smc/CheckSmcApplication.java
@@ -0,0 +1,13 @@
+package com.example.check_smc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckSmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckSmcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/check_smc/CheckSmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.check_smc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckSmcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
